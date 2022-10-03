8b91ed4b7ffba553e3b19439c7ac62470f28b2ce
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2022-10-03 14:39:17 -0300

    implement reactive repository

---
+++ src/main/java/com/calebe/reactiveexamples/PersonRepository.java
@@ -0,0 +1,11 @@
+package com.calebe.reactiveexamples;
+
+import com.calebe.reactiveexamples.domain.Person;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public interface PersonRepository {
+    Mono<Person> getById(Integer id);
+
+    Flux<Person> findAll();
+}
